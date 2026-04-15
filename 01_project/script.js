6b0c3c284f061181a1eacd56f0a678b37e0ff4d0
Vikaskanwate <vikaskanwate32@gmail.com>
2026-04-15 23:59:17 +0530

    practice

--- 01_project/script.js
+++ 01_project/script.js
@@ -1,3 +1,19 @@
 function addTask(){
+    const input = document.getElementById("taskInput");
+
+    let taskText = input.value.trim();
+
+    if(taskText === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    const ul = document.querySelector('ul');
+    ul.appendChild(li);
+
+    const deleteBtn = document.createElement("span");
+    deleteBtn.classList.add('delete-btn')
+    deleteBtn.textContent = "X"
+    li.appendChild(deleteBtn)
     
 }
